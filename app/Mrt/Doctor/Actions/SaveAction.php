44ff636f3f567b52448ed8a3b51f05b69c280f6c
Kuanysh <developerandroidkz@gmail.com>
2022-07-02 22:51:33 +0500

    save doctor info

---
+++ app/Mrt/Doctor/Actions/SaveAction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Mrt\Assistant\Actions;
+
+use App\Mrt\Doctor\Domain\Requests\SaveFormRequest as Request;
+use App\Mrt\Doctor\Domain\Services\SaveService as Service;
+use App\Responders\JsonResponder as Responder;
+
+class SaveAction
+{
+
+    public function __construct(Responder $responder, Service $service)
+    {
+        $this->responder = $responder;
+        $this->service = $service;
+    }
+
+    public function __invoke(Request $request)
+    {
+        return $this->responder->withResponse(
+            $this->service->handle($request->doctor_id, $request->validated())
+        )->respond();
+    }
+}
---
+++ app/Mrt/Doctor/Domain/Services/SaveService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Mrt\Doctor\Domain\Services;
+
+use App\Mrt\Doctor\Domain\Repositories\DoctorRepository as Repository;
+use App\Domain\Payloads\SuccessPayload;
+use App\Exceptions\MainException;
+
+class SaveService
+{
+
+    protected $repository;
+
+    public function __construct(Repository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function handle($doctor_id = 0, $data = [])
+    {
+        $doctor = $this->repository->updateById($doctor_id, $data);
+        if($doctor != null)
+            return new SuccessPayload(__("Doctor success saved"));
+
+        throw new MainException("Error to save Doctor");
+    }
+
+}
