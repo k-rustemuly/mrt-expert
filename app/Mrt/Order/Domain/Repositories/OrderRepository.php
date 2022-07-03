5686681c2be6b1007ea1253e50d79f612646e6bf
Kuanysh <developerandroidkz@gmail.com>
2022-07-03 23:50:04 +0500

    fix bug

--- app/Mrt/Order/Domain/Repositories/OrderRepository.php
+++ app/Mrt/Order/Domain/Repositories/OrderRepository.php
@@ -22,16 +22,17 @@ class OrderRepository extends Repository
         $query = $this->join('rb_order_status', $this->model->table.'.status_id', '=', 'rb_order_status.id')
         ->join('patient', $this->model->table.'.patient_id', '=', 'patient.id')
         ->join('reception', $this->model->table.'.reception_id', '=', 'reception.id')
-        ->select('rb_order_status.name_'.$this->language.' as status_name', 
-        'rb_order_status.color',
-        'patient.id as patient_id',
-        'patient.iin',
-        'patient.full_name as patient_name',
-        'patient.email',
-        'patient.phone_number',
-        'reception.full_name as reception_name',
-        $this->model->table.'.created_at',
-        $this->model->table.'.updated_at')
+        ->select($this->model->table.'.id',
+            'rb_order_status.name_'.$this->language.' as status_name', 
+            'rb_order_status.color',
+            'patient.id as patient_id',
+            'patient.iin',
+            'patient.full_name as patient_name',
+            'patient.email',
+            'patient.phone_number',
+            'reception.full_name as reception_name',
+            $this->model->table.'.created_at',
+            $this->model->table.'.updated_at')
         ->where($this->model->table.'.id', $id);
         $result =  $query->first();
         return $result ? $result->toArray() : [];
