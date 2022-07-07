4841bde35cab38dfed91a0f27a5d19f6409b8566
Kuanysh <developerandroidkz@gmail.com>
2022-07-08 03:06:48 +0500

    insert yo db

--- app/Mrt/Upload/Domain/Services/UploadService.php
+++ app/Mrt/Upload/Domain/Services/UploadService.php
@@ -4,9 +4,10 @@ namespace App\Mrt\Upload\Domain\Services;
 
 use App\Mrt\Upload\Domain\Repositories\UploadRepository as Repository;
 use App\Domain\Payloads\GenericPayload;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\File;
+use App\Exceptions\MainException;
 
 class UploadService
 {
@@ -24,21 +25,23 @@ class UploadService
         $unique_name = $file->hashName(); // Generate a unique, random name...
         $extension = $file->extension(); // Determine the file's extension based on the file's MIME type...
         $filepath = $this->generatePath();
-
-        File::streamUpload($filepath, $name, $file, false);
-        // $path = Storage::putFileAs($this->generatePath(), $file, $unique_name);
-        // $url = Storage::url($path);
-        // $size = Storage::size($path);
+        $config = Config::get('filesystems.disks.s3');
+        if(File::streamUpload($filepath, $unique_name, $file, false))
+        {
+        $path = $filepath."/".$unique_name;
+        $url = $config['url'].$path;
         $uuid = Str::orderedUuid();
-        // $upload_id = $this->repository->create([
-        //     "uuid" => $uuid,
-        //     "file_path" => $path,
-        //     "file_url" => $url,
-        //     "file_size" => $size,
-        //     "file_extension" => $extension
-        // ])["id"];
-        $upload_id = 0;
-        return new GenericPayload(["id" => $upload_id, "uuid" => $uuid]);
+        $upload_id = $this->repository->create([
+                "uuid" => $uuid,
+                "name" => $name,
+                "path" => $path,
+                "url" => $url,
+                "extension" => $extension
+            ])["id"];
+            
+            return new GenericPayload(["id" => $upload_id, "uuid" => $uuid]);
+        }
+        throw new MainException("Error to upload file");
     }
 
     private function generatePath():string
