57884660caeb8e78e0d65d2db642f66ac3a5ea1c
Kuanysh <developerandroidkz@gmail.com>
2022-07-11 14:24:58 +0500

    sdfsdf

--- app/Mrt/Upload/Actions/PdfAction.php
+++ app/Mrt/Upload/Actions/PdfAction.php
@@ -2,29 +2,21 @@
 
 namespace App\Mrt\Upload\Actions;
 
-use App\Mrt\Upload\Domain\Requests\UploadFormRequest as Request;
-use App\Mrt\Upload\Domain\Services\LocalService as Service;
 use App\Responders\JsonResponder as Responder;
-use Illuminate\Support\Facades\App;
-use Barryvdh\DomPDF\Facade\Pdf;
 use App\Domain\Payloads\GenericPayload;
 
 class PdfAction
 {
 
-    public function __construct(Responder $responder, Service $service)
+    public function __construct(Responder $responder)
     {
         $this->responder = $responder;
-        $this->service = $service;
     }
 
-    public function __invoke(Request $request)
+    public function __invoke()
     {
-        
-    //$pdf = Pdf::loadView('conclusion', array("aaa"));
-    return $this->responder->withResponse(
-        new GenericPayload("aaa")
-    )->respond();
-    //return $pdf->save('invoice.pdf');
+        return $this->responder->withResponse(
+            new GenericPayload()
+        )->respond();
     }
 }
