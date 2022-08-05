2e129f00b196c13c89064af53439b58652944285
Kuanysh <developerandroidkz@gmail.com>
2022-08-05 22:59:33 +0500

    gf

--- app/Mrt/Reception/Domain/Services/SaveService.php
+++ app/Mrt/Reception/Domain/Services/SaveService.php
@@ -5,6 +5,7 @@ namespace App\Mrt\Reception\Domain\Services;
 use App\Mrt\Reception\Domain\Repositories\ReceptionRepository as Repository;
 use App\Domain\Payloads\SuccessPayload;
 use App\Exceptions\MainException;
+use Illuminate\Support\Facades\Hash;
 
 class SaveService
 {
@@ -18,13 +19,16 @@ class SaveService
 
     public function handle($reception_id = 0, $data = [])
     {
-        $user = auth('branch_admin')->userOrFail();
+        $user = auth('branch_admin')->user();
         $reception = $this->repository->getById($reception_id);
         if($reception->branch_id != $user->branch_id)
         {
             throw new MainException("Error to save Reception");
         }
-
+        if(isset($data["password"]))
+        {
+            $data["password"] = Hash::make($data["password"]);
+        }
         $reception = $this->repository->updateById($reception_id, $data);
         if($reception != null)
             return new SuccessPayload(__("Reception success saved"));
