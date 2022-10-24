52d5d6b3eebd2485a1a012398538586dca3bf115
Kuanysh <developerandroidkz@gmail.com>
2022-10-25 00:11:29 +0500

    fix bugs schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,8 +18,16 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
-            $orders = DB::table('orders')->select('id')->where('status_id', 1)->where('created_at', '<=', Carbon::yesterday())->whereNotIn('id', DB::table('suborders')->select(DB::raw('DISTINCT(order_id)')))->get()->all();
-            DB::table('orders')->whereIn('id', $orders)->where('status_id', 1)->delete();
+            $orders = DB::table('orders')
+                            ->select('id')
+                            ->where('status_id', 1)
+                            ->where('created_at', '<=', Carbon::yesterday())
+                            ->whereNotIn('id', DB::table('suborders')
+                                                    ->select(DB::raw('DISTINCT(order_id)'))
+                                        )
+                            ->get()
+                            ->all();
+            DB::table('orders')->where('status_id', 1)->whereIn('id', $orders)->delete();
         })->daily();
     }
 
