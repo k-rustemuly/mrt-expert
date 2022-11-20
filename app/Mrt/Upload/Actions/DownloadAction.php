adbfbe2cb80a4999c9bef726393e5b486adc9454
Kuanysh <developerandroidkz@gmail.com>
2022-11-21 01:05:04 +0500

    gfd

--- app/Mrt/Upload/Actions/DownloadAction.php
+++ app/Mrt/Upload/Actions/DownloadAction.php
@@ -3,6 +3,7 @@
 namespace App\Mrt\Upload\Actions;
 
 use App\Domain\Requests\DefaultRequest as Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\File;
 
 class DownloadAction
@@ -13,6 +14,14 @@ class DownloadAction
 
     public function __invoke(Request $request)
     {
+        return redirect(Storage::disk('s3')->temporaryUrl(
+            $request->path,
+            now()->addMinutes(5),
+            [
+                'ResponseContentType' => 'application/octet-stream',
+                'ResponseContentDisposition' => 'attachment; filename="'.utf8_encode($request->name).'"',
+            ]
+            ));
         return redirect(File::streamDownload($request->path, $request->name));
     }
 }
