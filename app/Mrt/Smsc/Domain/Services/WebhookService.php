4b74f205090a9a55012309b7f889cc18e99c5e37
Kuanysh <developerandroidkz@gmail.com>
2022-11-27 22:36:28 +0500

    test

--- app/Mrt/Smsc/Domain/Services/WebhookService.php
+++ app/Mrt/Smsc/Domain/Services/WebhookService.php
@@ -3,12 +3,14 @@
 namespace App\Mrt\Smsc\Domain\Services;
 
 use App\Domain\Payloads\SuccessPayload;
+use Illuminate\Support\Facades\Log;
 
 class WebhookService
 {
 
     public function handle($data = [])
     {
+        Log::debug(implode(",", $data));
         return new SuccessPayload(__("New admin success added"), $data);
     }
 
